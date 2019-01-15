f6f234f5ed625e8c0791854873ff3be10372c0ab
avastermark19 <30867791+avastermark19@users.noreply.github.com>
2019-01-15 13:31:10 -0600

    Add files via upload

--- vignettes/my-vignette.R
+++ vignettes/my-vignette.R
@@ -8,6 +8,7 @@ knitr::opts_chunk$set(
 
 library(mclust)
 library(TestPackage12)
+library(edgeR)
 
 #MyVignette
 
@@ -17,8 +18,10 @@ xassetCountrySector <- read.table('~/TestPackage12/extdata/format.out.Heliconius
   counts.head   <- as.matrix(round(xassetCountrySector[3:ncol(xassetCountrySector)] ))
   Groupings <- rep("X", ncol(xassetCountrySector)-2 )
   segLengths <- xassetCountrySector$Len
-  cd.head <- new("countData", data = counts.head, replicates = Groupings, groups = list(Groupings, Groupings), annotation = gene.chr.match, seglens = segLengths  )
-  libsizes(cd.head) <-   getLibsizes(cd.head,   estimationType = "edgeR")
+  cd.head <- new("countDat", data = counts.head, replicates = factor(Groupings), groups = list(Groupings, Groupings), annotation = gene.chr.match  )
+  cd.head@rowObservables$seglens = segLengths
+  #libsizes(cd.head) <-   getLibsizes(cd.head,   estimationType = "edgeR")
+  libsizes(cd.head) <-   unname(getLibsizes2(cd.head,   estimationType = "edgeR"))
 OBJ <- cd.head 
 
 OBJ@replicates<- as.factor(c("F","F", "F", "M", "M", "M","M", "M", "M", "M", "M","M", "F", "F", "F", "F","F", "F", "F", "F", "M","M", "F","F", "F", "F","M", "M", "F", "F", "F","F", "M", "M", "M", "M","M", "M", "M", "M", "F","F", "F", "F", "M", "M","M", "M", "F", "F", "F","F", "F", "F", "M", "M","M", "M"))
@@ -37,15 +40,15 @@ CYD_HD <- OBJ[,c(40,42,44,46,48,50,52,54,56,58)]
 plotExpr(CYD_AB, col=c("black","red","black","red"), notch=T, outline=FALSE, cex.axis=0.8, mode_mean=FALSE, LOG2=TRUE, clusterby_grouping=FALSE, groupings="something")
 
 # REMOVING 28%
-f_CYD_AB <- dafsFilterWrapper(CYD_AB)
+f_CYD_AB <- dafsFilter(CYD_AB)
 plotExpr(f_CYD_AB, col=c("black","red","black","red"), notch=T, outline=FALSE, cex.axis=0.8, mode_mean=FALSE, LOG2=TRUE, clusterby_grouping=FALSE, groupings="something")
 
 # REMOVING 56%
-f_CYD_AB <- quantFilter(CYD_AB, 0.2) 
+f_CYD_AB <- quantFilter(CYD_AB, lo.bound=0.2) 
 plotExpr(f_CYD_AB, col=c("black","red","black","red"), notch=T, outline=FALSE, cex.axis=0.8, mode_mean=FALSE, LOG2=TRUE, clusterby_grouping=FALSE, groupings="something")
 
 # REMOVING 71%
-f_CYD_AB <- iqrxFilter(CYD_AB, IQR_MULTIPLIER=1)
+f_CYD_AB <- iqrxFilter(CYD_AB, iqr_multi = 1.5)
 plotExpr(f_CYD_AB, col=c("black","red","black","red"), notch=T, outline=FALSE, cex.axis=0.8, mode_mean=FALSE, LOG2=TRUE, clusterby_grouping=FALSE, groupings="something")
 
 outlist <- generateStats(f_CYD_AB, groupings="something", mode_mean=TRUE)
@@ -60,5 +63,9 @@ plotRatioBoxes(f_CYD_AB, treatment1="M", treatment2="F", groupings="something", 
 
 plotRatioDensity(f_CYD_AB, treatment1="M", treatment2="F", mode_mean=TRUE, LOG2=TRUE, col =c("black","red"), lty = 1, type = "l", groupings="something")
 
+
+LEG <- OBJ[,c(2,5,8,11,14,17)]
+dm<- cD.DM(LEG)
+#glSeq(dm, "-1 + replicate*something") # TOO SLOW TO RUN EVERY TIME..
 
 
